0d21d0eb101db51e83a54c1a8661cc6776b5bf2a
Augus1000 <45717621+Augus1000@users.noreply.github.com>
2022-01-15 11:25:46 -0400

    Update resume.component.ts

--- src/app/components/resume/resume.component.ts
+++ src/app/components/resume/resume.component.ts
@@ -140,8 +140,214 @@ export class ResumeComponent implements OnInit {
       console.error('Error al recoger los registros:' + error);
 
     })
+this.oestados=this.aService.getOestados();
+    this.obranches=this.aService.getObranches();
+    // Si estoy editando
+    if (this.registrySelected) {
+      // Validamos la cantidad
+      // Ponemos el id tambien para poder recogerlo despues
+      this.formRegistry = this.formBuilder.group({
+        descripcion: new FormControl(this.registrySelected.descripcion),
+        date: new FormControl(new Date(this.registrySelected.date)),
+        type: new FormControl(this.registrySelected.type),
+        precio: new FormControl(this.registrySelected.precio),
+        id: new FormControl(this.registrySelected.id),
+        user: new FormControl(this.registrySelected.user),
+        catastralcode: new FormControl(this.registrySelected.catastralcode),
+        ruat: new FormControl(this.registrySelected.ruat),
+        internalcode: new FormControl(this.registrySelected.internalcode),
+        depreciation: new FormControl(this.registrySelected.depreciation),
+        ubication: new FormControl(this.registrySelected.ubication),
+        responsable: new FormControl(this.registrySelected.responsable),
+        licenseplate: new FormControl(this.registrySelected.licenseplate),
+        typeofvehicle: new FormControl(this.registrySelected.typeofvehicle),
+        marca: new FormControl(this.registrySelected.marca),
+        color: new FormControl(this.registrySelected.color),
+        chassis: new FormControl(this.registrySelected.chassis),
+        model: new FormControl(this.registrySelected.model),
+        agencia:new FormControl(this.aService.getObranches),
+        idActivofijo: new FormControl(this.aService.getAtypes),
+        invoice: new FormControl(this.registrySelected.invoice),
+        provider: new FormControl(this.registrySelected.provider),
+        fiscal: new FormControl(this.registrySelected.fiscal),
+        authNumber: new FormControl(this.registrySelected.authNumber),
+        imagePost:new FormControl(this.registrySelected.imagePost),
+        qrcode:new FormControl(''),
+        estado:new FormControl(this.aService.getOestados)
+      });
+    } else {
+      // Nuevo registro
+      // Validamos la cantidad
+      this.formRegistry = this.formBuilder.group({
+        descripcion: new FormControl('', Validators.required),
+        date: new FormControl(new Date()),
+        type: new FormControl(this.typeRegistry),
+        id:new FormControl(0),
+        idActivofijo: new FormControl('',[Validators.required,letterValid]),
+        estado: new FormControl('',[Validators.required,letterValid]),
+        precio: new FormControl('', [Validators.required, quantityValid]),
+        catastralcode: new FormControl(''),
+        ruat: new FormControl(''),      
+        internalcode: new FormControl('', [quantityValid, internalExists(this.listRegistries)]),
+        depreciation: new FormControl('', [Validators.required, quantityValid]),
+        ubication: new FormControl('', Validators.required),
+        responsable: new FormControl('', [Validators.required,letterValid]),
+        licenseplate: new FormControl(''),
+        typeofvehicle: new FormControl(''),
+        marca: new FormControl(''),
+        color: new FormControl(''),
+        chassis: new FormControl(''),
+        model: new FormControl(''),
+        invoice: new FormControl('', [Validators.required, quantityValid, lengthnineValid,invoiceExists(this.listRegistries)]),
+        provider: new FormControl('',[Validators.required,letterValid]),
+        fiscal: new FormControl('',[Validators.required, quantityValid]),
+        authNumber: new FormControl('', [Validators.required, quantityValid, lengththirteenValid]),
+        imagePost:new FormControl('', Validators.required),
+        qrcode:new FormControl('')
+
+      });
+
+
+
+    };
+   
+    // Obtengo los activos
+    this.aService.getActivofijo().subscribe(listaActivosfijos => {
+      this.listaActivosfijos= listaActivosfijos;
+
+      this.cargarActivosfijos = true;
+    }, error => {
+      console.error(error);
+      this.cargarActivosfijos= true;
+    });
+    this.aService.getBranches().subscribe(listBranches => {
+      this.listBranches = listBranches;
+
+      this.loadBranches = true;
+    }, error => {
+      console.error(error);
+      this.loadBranches = true;
+    });
+    this.aService.getEstados().subscribe(listEstados => {
+      this.listEstados = listEstados;
+  
+      this.loadEstados = true;
+    }, error => {
+      console.error(error);
+      this.loadEstados = true;
+    });
+  }
+  
+  async onFileChanged(event) {
+    const file = event.target.files[0];
+    if (file) {
+       const filePath = `${this.basePath1}/${file.name}`;
+       this.task =  this.storage.upload(filePath, file);
+
+
+       this.progressValue = this.task.percentageChanges();       // <<<<< Percentage of uploading is given
+
+
+    (await this.task).ref.getDownloadURL().then(url => {this.downloadableURL = url; });
+
+     } else {
+       alert('No images selected');
+       this.downloadableURL = ''; }
+
+     }
+
+  /**
+   * Obtengo el formcontrol price
+   */
+  get precio() {
+    return this.formRegistry.get('precio');
 
   }
+/**
+   * Obtengo el formcontrol internalcode
+   */
+  get internalcode() {
+    return this.formRegistry.get('internalcode');
+  }
+  /**
+   * Obtengo el formcontrol invoice
+   */
+  get invoice() {
+    return this.formRegistry.get('invoice');
+  }
+  /**
+   * Obtengo el formcontrol responsable
+   */
+  get responsable() {
+    return this.formRegistry.get('responsable');
+  }
+/**
+   * Obtengo el formcontrol depreciation
+   */
+  get depreciation() {
+    return this.formRegistry.get('depreciation');
+  }
+  /**
+   * Obtengo el formcontrol provider
+   */
+  get provider() {
+    return this.formRegistry.get('provider');
+  }
+  /**
+   * Obtengo el formcontrol type
+   */
+  get type() {
+    return this.formRegistry.get('type');
+  }
+   
+  /**
+   * Obtengo el formcontrol idActivofijo
+   *
+  
+   * Obtengo el formcontrol agencia
+   */
+  get agencia() {
+    return this.formRegistry.get('agencia');
+  }
+  /**
+   * Obtengo el formcontrol ubication
+   */
+  get ubication(){
+    return this.formRegistry.get('ubication');
+  }
+/**
+   * Obtengo el formcontrol date
+   */
+  get date(){
+    return this.formRegistry.get('date');
+  }
+  /**
+   * Obtengo el formcontrol fiscal
+   */
+  get fiscal(){
+    return this.formRegistry.get('fiscal');
+  }
+  /**
+   * Obtengo el formcontrol authNumber
+   */
+  get authNumber(){
+    return this.formRegistry.get('authNumber');
+  }
+
+get estado(){
+  return this.formRegistry.get('estado')
+}
+
+  /**
+   * Cierro el detalle
+   * @param $event Estado de la ventana
+   */
+  closeDetail($event) {
+    this.close.emit($event);
+  
+
+  }
+  
 
 
   /**
@@ -230,7 +436,76 @@ export class ResumeComponent implements OnInit {
     })
 
   } */
+ errorMessage = "";
+  
+  addBackup() {
 
+      // Devuelvo una promesa
+    
+    this.rService.getRegistries().subscribe(list => {
+      // Ordeno las listas al reves y las clono
+      
+      this.listRegistriesFiltered = cloneDeep(orderBy(list, r => r.date).reverse());
+
+      
+      
+      
+    let backup = new Registry(this.listRegistriesFiltered);
+    
+      
+      
+      this.rService.addBackup(backup).then(() => {
+        this.modalService.open(this.modal_success).result.then(() => {
+          this.parentDetail.closeDetail()
+          this.errorMessage = ""
+        })
+        
+        
+    
+      
+      }, error => {
+        if(error && error.message && error.message.includes('PERMISSION_DENIED')){
+          this.errorMessage = 'User do not have permission'
+        }else{
+          this.errorMessage = "Usuario no autorizado";
+        }
+        this.modalService.open(this.modal_error);
+        
+      })  
+    })
+  }
+ 
+addRegistry() {
+ 
+     let registry = new Registry(this.formRegistry.value);
+ 
+ 
+     if (this.registrySelected) {
+       // Editar registro
+       registry.qrcode=this.qrcodescanned;
+       registry.imagePost = this.downloadableURL;
+       this.rService.editRegistry(registry).then(() => {
+         this.modalService.open(this.modal_success).result.then(() => {
+           this.parentDetail.closeDetail();
+         })
+ 
+       },
+       );
+       
+     } else {
+       // Crear registro
+        error => {
+         if(error && error.message && error.message.includes('PERMISSION_DENIED')){
+           this.errorMessage = 'User do not have permission'
+         }else{
+           this.errorMessage = "Usuario no autorizado";
+         }
+         this.modalService.open(this.modal_error);
+         this.parentDetail.closeDetail();
+       } 
+  
+   }
+ }
 
   /**
    * Manda el registro a la cabecera para que lo abra
@@ -239,7 +514,11 @@ export class ResumeComponent implements OnInit {
   openEditDetail(registry: Registry) {
     this.rService.selectRegistry(registry);
   }
-
+ openAsignationDetail(registry: Registry) { 
+       
+    this.modalService.open(this.modal_responsable);
+   
+  }
   /**
    * Devuelve el resultado del filtro
    * @param $event Lista de registros filtrados
